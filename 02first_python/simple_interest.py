349e565035685af29aa78327f8cb5a179fd06db2
jayanthmajji <jayanthmajji45@gmail.com>
2024-05-15 16:59:39 +0530

    Simple interest program

---
+++ 02first_python/simple_interest.py
@@ -0,0 +1,6 @@
+p = int(input("enter the principal ammount:"))
+t = int(input("enter the time:"))
+r = int(input("enter the rate of interest:"))
+
+sip = p * t * r/100
+print("Simple interest is:", sip)

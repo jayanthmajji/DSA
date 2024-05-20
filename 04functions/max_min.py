05c1091fd0e8ea76090aa6f4ad052a6d0f220868
jayanthmajji <jayanthmajji45@gmail.com>
2024-05-20 18:00:44 +0530

    max of the three numbers

---
+++ 04functions/max_min.py
@@ -0,0 +1,10 @@
+def max(a, b, c):
+    if a > b and c:
+        print("The max number is :", a)
+    elif b > c and a:
+        print("The max number is :", b)
+    else:
+        print("The max number is :", c)
+
+
+print(max(2, 3, 4))
